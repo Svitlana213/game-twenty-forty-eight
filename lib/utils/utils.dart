a8c1fc23d50a03414e5d898958b984ecef32e8bd
sbondar <bondar.svitlana@yahoo.com>
2024-04-01 21:27:39 +0300

    colors

---
+++ lib/utils/utils.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+final Map<int,Color> tileColors = <int, Color>{
+  2: Colors.white24,
+  4: Colors.grey,
+  8: Colors.blueGrey,
+  16: Colors.brown,
+  32: Colors.blue,
+  64: Colors.greenAccent,
+  128: Colors.blueAccent,
+  256: Colors.indigo,
+  512: Colors.white70,
+  1024: Colors.amberAccent,
+  2048: Colors.orange,
+};
