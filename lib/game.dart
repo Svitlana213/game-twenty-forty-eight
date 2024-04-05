c2c5e67e711d4e50de33d7272ebdc8cf0fe13795
sbondar <bondar.svitlana@yahoo.com>
2024-04-05 22:02:19 +0300

    update ui

--- lib/game.dart
+++ lib/game.dart
@@ -141,9 +141,16 @@ class _BoardWidgetState extends State<BoardWidget> {
                         Text('Score: ${_board.score.toString()}', style: TextStyle(color: Colors.black, fontSize: 24),),
                         ElevatedButton(
                           onPressed: () {
+
                             newGame();
                           },
-                          child: Text('New game'),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Color.fromARGB(250, 143, 122, 102),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0)
+                            )
+                          ),
+                          child: Text('New game', style: TextStyle(color: Colors.white, fontSize: 18),),
                         ),
                       ],
                     )
@@ -216,28 +223,37 @@ class _BoardWidgetState extends State<BoardWidget> {
 class EndGameScreen {
   static void show(BuildContext context, _BoardWidgetState state){
     showDialog(
-        context: context, 
-        builder: (context) => Container(
-          width: 50,
-          height: 50,
-          child: AlertDialog(
-            backgroundColor: Colors.grey,
-            alignment: Alignment.center,
-            title: Text('Game over', style: TextStyle(color: Colors.black),),
-            actions: [
-              ElevatedButton(
-                onPressed: () {
-                  state.newGame();
-                  Navigator.pop(context);
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                ),
-                child: Text('New game', style: TextStyle(color: Colors.black)),
+        context: context,
+        builder: (context) => Material(
+          color: Color.fromRGBO(255, 255, 255, 0.2),
+            child: Container(
+              width: 50,
+              height: 50,
+              child: AlertDialog(
+                backgroundColor: Colors.white,
+                alignment: Alignment.center,
+                title: Text('Game over', style: TextStyle(color: Colors.black), textAlign: TextAlign.center,),
+                actions: [
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        state.newGame();
+                        Navigator.pop(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(250, 143, 122, 102),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0)
+                          )
+                      ),
+                      child: Text('New game', style: TextStyle(color: Colors.white, fontSize: 18)),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        )
+            )
+        ),
+
     );
   }
 }
